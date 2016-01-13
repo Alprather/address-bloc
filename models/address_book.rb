6eb1dab3c5997912a13f95f418e1ab216fefbb27
Alicia Prather <Alprather@gmail.com>
2016-01-13 16:33:16 +0400

    checkpoint 20 complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,4 @@
-# #8
+
  require_relative 'entry'
 
 class AddressBook
@@ -8,16 +8,16 @@ class AddressBook
     @entries = []
   end
   def add_entry(name, phone_number, email)
-# #9
+
     index = 0
     @entries.each do |entry|
-# #10
+
       if name < entry.name
         break
       end
       index += 1
     end
-# #11
+
     @entries.insert(index, Entry.new(name, phone_number, email))
   end
   def remove_entry(name, phone_number, email)
